c73105e1594e3e21e5941c46177400c670b5a060
hoque95 <hoque95@gmail.com>
2019-10-20 15:31:47 -0400

    mY first Commit

---
+++ src/usa/Dhaka.java
@@ -0,0 +1,12 @@
+package usa;
+
+//import sunDay.NewYork;
+
+public class Dhaka {
+
+	public static void main(String[] args) {
+		NewYorkPA myParameter =new NewYorkPA ("Alvee Hoque","Bangladesh","01-29-1995",5);
+
+	}
+
+}
---
+++ src/usa/NewYorkPA.java
@@ -0,0 +1,14 @@
+package usa;
+
+public class NewYorkPA {
+	public NewYorkPA(String newName,String newCountry,String newDob, int newHeight) {
+		//System.out.println ("Name of the Person:"  +newName+  ", "  "Country of Origin:"  +newCountry+  "," +"Date of Birth :" +newDob+ ", " +"Height of the Person :"+ newHeight);
+		 
+		
+		  System.out.println("Name of the Person:" +newName);
+		  System.out.println("Country of Origin: " +newCountry);
+		  System.out.println("Date of Birth :" +newDob);
+		  System.out.println("Height of the Person :"+ newHeight);
+		 
+}
+}
